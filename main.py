24df6504c5664ca0e6964591fd0b4adb838be2de
Anpilov Andrey <a.anpilov93@gmail.com>
2024-10-22 13:16:38 +0300

    refactoring

--- main.py
+++ main.py
@@ -4,37 +4,38 @@ import ptbot
 import config
 
 
-BOT = ptbot.Bot(config.TG_TOKEN)
-
-
 def message_for_timer(chat_id, message):
     message = "Время вышло"
-    
-    BOT.send_message(chat_id=chat_id, message=message)
-
-
-def timer(chat_id, message):
-    time = parse(message)
-
-    BOT.create_timer(time, message_for_timer, chat_id=chat_id, message=message)
+    bot.send_message(chat_id=chat_id, message=message)
 
 
 def countdown(chat_id, message):
     time = parse(message)
-
     main_notify_message = "Осталось {} секунд!".format(time)
-    message_id = BOT.send_message(chat_id=chat_id, message=main_notify_message)
-    
+    message_id = bot.send_message(chat_id=chat_id, message=main_notify_message)
 
-    BOT.create_countdown(time, notify_progress, chat_id=chat_id, message_id=message_id)
-    BOT.create_timer(time, message_for_timer, chat_id=chat_id, message=message)
+    bot.create_countdown(
+        time,
+        notify_progress,
+        chat_id=chat_id,
+        message_id=message_id
+        )
+    bot.create_timer(time, message_for_timer, chat_id=chat_id, message=message)
 
 
 def notify_progress(secs_left, chat_id, message_id):
-    bar = render_progressbar(total=secs_left, iteration=1)
+    bar = render_progressbar
 
-    main_notify_message = "Осталось {} секунд! \n {}".format(secs_left, bar)
-    BOT.update_message(chat_id=chat_id, message_id=message_id, new_message=main_notify_message)
+    if secs_left != 0:
+        notify_message = f"Осталось {secs_left} секунд! \n {bar(secs_left, 0.9)}"
+    else:
+        notify_message = f"Осталось {0} секунд! \n {bar(1, 1)}"
+
+    bot.update_message(
+        chat_id=chat_id,
+        message_id=message_id,
+        new_message=notify_message
+        )
 
 
 def render_progressbar(total, iteration, prefix='', suffix='', length=30, fill='█', zfill='░'):
@@ -47,10 +48,11 @@ def render_progressbar(total, iteration, prefix='', suffix='', length=30, fill='
 
 
 def main():
-    BOT.send_message(config.TG_CHAT_ID, "Бот запущен")
-    BOT.reply_on_message(countdown)
-    BOT.run_bot()
+    bot.send_message(config.TG_CHAT_ID, "Бот запущен")
+    bot.reply_on_message(countdown)
+    bot.run_bot()
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
+    bot = ptbot.Bot(config.TG_TOKEN)
     main()
